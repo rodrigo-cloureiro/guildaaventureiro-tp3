ad238bef85e08ace5aab57f499d75d6d4470f12d
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-02 14:07:49 -0300

    cria o service PainelTaticoMissaoService para listar as missoes relevantes dos ultimos X dias

---
+++ src/main/java/br/com/infnet/guildaaventureiro/service/PainelTaticoMissaoService.java
@@ -0,0 +1,25 @@
+package br.com.infnet.guildaaventureiro.service;
+
+import br.com.infnet.guildaaventureiro.dto.missao.TopMissoesResponse;
+import br.com.infnet.guildaaventureiro.repository.operacoes.PainelTaticoMissaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PainelTaticoMissaoService {
+    private final PainelTaticoMissaoRepository painelTaticoMissaoRepository;
+
+    // ===========
+    // Top X dias
+    // ===========
+    public List<TopMissoesResponse> topMissoesDias(int dias) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime cutoffDate = now.minusDays(dias);
+
+        return painelTaticoMissaoRepository.top15Dias(cutoffDate, now);
+    }
+}
