9f29911a6c9392d32ad0d40f31a0d58e6aca53b4
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-04-08 23:01:57 -0300

    remove DTOs nao utilizados

--- src/main/java/br/com/infnet/guildaaventureiro/dto/elastic/CategoriaAggregation.java
+++
@@ -1,7 +0,0 @@
-package br.com.infnet.guildaaventureiro.dto.elastic;
-
-public record CategoriaAggregation(
-        String categoria,
-        long quantidade
-) {
-}
--- src/main/java/br/com/infnet/guildaaventureiro/dto/elastic/RaridadeAggregation.java
+++
@@ -1,7 +0,0 @@
-package br.com.infnet.guildaaventureiro.dto.elastic;
-
-public record RaridadeAggregation(
-        String raridade,
-        long quantidade
-) {
-}
